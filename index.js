e09563edeb4e71def6698332e95ad1af4a8b5fdb
shellyb-Online <shellybanahene@gmail.com>
2024-10-02 17:51:31 +0000

    todo-api updated

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import 'dotenv/config'
 import todoRouter from './routes/todo.js';
 import userRouter from './routes/user.js';
 
 // connect to database
-await mongoose.connect('mongodb+srv://todo-api:todo-api@todoapi.elum6.mongodb.net/todo-db?retryWrites=true&w=majority&appName=todoapi');
+await mongoose.connect(process.env.MONGO_URL);
 
 // Create an express app
 const app = express();
