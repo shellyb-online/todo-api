71c1f4bd72cb2e3a27bf0ac1ddce918ec9c9eaad
shellyb-Online <shellybanahene@gmail.com>
2024-10-02 12:11:20 +0000

    mongoose added

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import todoRouter from './routes/todo.js';
+import userRouter from './routes/user.js';
+
+// connect to database
+await mongoose.connect('mongodb+srv://todo-api:todo-api@todoapi.elum6.mongodb.net/todo-db?retryWrites=true&w=majority&appName=todoapi');
 
 // Create an express app
 const app = express();
 
 // Define routes
-app.get('/hello', function(req, res, next) {
-    console.log(req.headers);
-    res.json('you visited the hello endpoint!');
-
-});
-
+app.use(todoRouter);
+app.use(userRouter); 
 // Define routes
-app.get('/goodbye',(req, res, next) => {
-    console.log(req.query);
-    res.json('same to you!'); 
-})
+// app.get('/goodbye',(req, res, next) => {
+//     console.log(req.query);
+//     res.json('same to you!'); 
+// });
 
-// Listen for incoming requests
-app.listen(3000, function(){
+// listen for incoming requests
+app.listen(3000,() => {
     console.log('App is listening on port 3000');
-}); 
-
+});
